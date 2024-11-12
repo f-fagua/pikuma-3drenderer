7293e7b6d6b23e993198c6069460d115de7163a9
Felipe Fagua <felipe.fagua@unity3d.com>
2024-11-12 13:48:01 -0500

    Matrix Typedef

--- 3drenderer/src/main.c
+++ 3drenderer/src/main.c
@@ -151,9 +151,8 @@ void update(void)
 		{
 			vec3_t transformed_vertex = face_vertices[j];
 			
-			transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
-			transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
-			transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.z);
+			// TODO: Use a matrix to scale our original vertex
+			
 			
 			// Translate the vertex away from the camera
 			transformed_vertex.z += 5;
--- untitled.c
+++ untitled.c
@@ -1,5 +1,22 @@
-// Draw flat-bottom filled triangle
-void draw_flat_bottom(float x0, float y0, float x1, float y1, float Mx, float My);
 
-// Draw flat-top filled triangle
-void draw_flat_bottom(float x1, float y1, float Mx, float My, float x2, float y2);
+
+
+
+typedef struct {
+    float m[4][4];
+} mat4_t;
+
+
+mat4_t mat4_make_scale (float sx, float sy, float sz)
+{
+	// | sx  0  0  0 |
+	// |  0 sy  0  0 |
+	// |  0  0 sz  0 |
+	// |  0  0  0  1 |
+
+	mat4_t m = mat4_identity();
+	m.m[0][0] = sx;
+	m.m[1][1] = sy;
+	m.m[2][2] = sz;
+	return m;
+}
