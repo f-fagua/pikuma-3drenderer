a601c21eed5cce7e11f0baf635d64f335f6d5c6e
Felipe Fagua <felipe.fagua@unity3d.com>
2024-11-20 18:14:24 -0500

    The LookAt Function

--- untitled.c
+++ untitled.c
@@ -1,32 +1,28 @@
-///////////////////////////////////////////////////////////////////////////////
-// Return the barycentric wights alpha, beta, and gamm a for point p
-///////////////////////////////////////////////////////////////////////////////
-vect_3 barycentric_weights(vect_2 a, vect_2 b, vect_2 c, vect_2 p) 
+typedef struct 
 {
-	// Find the vectors between the vertices ABC and point p
-	vect_2 ac = vec2_sub(c, a);
-	vect_2 ab = vec2_sub(b, a);
-	vect_2 pc = vec2_sub(c, p);
-	vect_2 pb = vec2_sub(b, p);
-	vect_2 ap = vec2_sub(p, a);
+	vec3_t position;
+	vec3_t direction;
+} camera_t;
 
-	// Find area of the full parallelogram (triangle ABC) using cross product 
-	float area_parallelogram_abc = (ac.x * ab.y - ac.y * ab.x); // || AC x AB ||
 
-	// Find area of the full parallelogram (triangle PBC) using cross product
-	float area_parallelogram_pbc = (pc.x * pb.y - pc.y * pb.x); // || PC x PB ||
+mat4_t mat4_look_at(vec3_t eye, vec3_t target, vec3_t up) 
+{
+	vec3_t z = vec3_normalize(vec3_sub(target, eye));	// forward (z) vector
+	vec3_t x = vec3_normalize(vec3_cross(up, z));		// right (x) vector
+	vec3_t y = vec3_cross(z, x);						// up (y) vector
 
-	// Find area of the full parallelogram (triangle APC) using cross product
-	float area_parallelogram_apc = (ac.x * ap.y - ac.y * ap.x); // || AC x AP ||
+	// | x.x  x.y  x.z  -dot(x, eye) |
+	// | y.x  y.y  y.z  -dot(y, eye) |
+	// | z.x  z.y  z.z  -dot(x, eye) |
+	// |   0    0    0             1 |
 
-	// Alpha = area of parallelogram-PBC over the area of the full parallelogram-ABC
-	float alpha = area_parallelogram_pbc / area_parallelogram_abc;
+	mat4_t view_matrix = 
+	{{
+		{ x.x, x.y, x.z, -vec3_dot(x, eye) },
+		{ y.x, y.y, y.z, -vec3_dot(y, eye) },
+		{ z.x, z.y, z.z, -vec3_dot(z, eye) },
+		{   0,   0,   0, 				 1 }
+	}};
 
-	// Beta = area of parallelogram-APB over the area of the full parallelogram-ABC
-	float beta = area_parallelogram_apc / area_parallelogram_abc;
-
-	float gamma = 1-alpha-beta;
-
-	vect_3 weights = {alpha, beta, gamma};
-	return weights;
+	return view_matrix;
 }
