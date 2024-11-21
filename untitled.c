0d315f3e3b086beca3701857812d00624a88005c
f-fagua <felipe.fagua@unity3d.com>
2024-11-21 05:41:56 -0500

    Defining Frustum Planes Points & Normals

--- untitled.c
+++ untitled.c
@@ -1,17 +1,32 @@
-
-
-enum 
+void initialize_frustrum_planes(float fov, float z_near, float z_far) 
 {
-	LEFT_FRUSTUM_PLANE,
-	RIGHT_FRUSTUM_PLANE,
-	TOP_FRUSTUM_PLANE,
-	BOTTOM_FRUSTUM_PLANE,
-	NEAR_FRUSTUM_PLANE,
-	FAR_FRUSTUM_PLANE
+	frustrum_planes[LEFT_FRUSTUM_PLANE].point = {0, 0, 0};
+	frustrum_planes[LEFT_FRUSTUM_PLANE].normal.x = cos(fov/2);
+	frustrum_planes[LEFT_FRUSTUM_PLANE].normal.y = 0;
+	frustrum_planes[LEFT_FRUSTUM_PLANE].normal.z = sin(fov/2);
+
+	frustrum_planes[RIGHT_FRUSTUM_PLANE].point = {0, 0, 0};
+	frustrum_planes[RIGHT_FRUSTUM_PLANE].normal.x = -cos(fov/2);
+	frustrum_planes[RIGHT_FRUSTUM_PLANE].normal.y = 0;
+	frustrum_planes[RIGHT_FRUSTUM_PLANE].normal.z = sin(fov/2);
+
+	frustrum_planes[TOP_FRUSTUM_PLANE].point = {0, 0, 0};
+	frustrum_planes[TOP_FRUSTUM_PLANE].normal.x = 0;
+	frustrum_planes[TOP_FRUSTUM_PLANE].normal.y = -cos(fov/2);
+	frustrum_planes[TOP_FRUSTUM_PLANE].normal.z = sin(fov/2);
+
+	frustrum_planes[BOTTOM_FRUSTUM_PLANE].point = {0, 0, 0};
+	frustrum_planes[BOTTOM_FRUSTUM_PLANE].normal.x = 0;
+	frustrum_planes[BOTTOM_FRUSTUM_PLANE].normal.y = cos(fov/2);
+	frustrum_planes[BOTTOM_FRUSTUM_PLANE].normal.z = sin(fov/2);
+
+	frustrum_planes[NEAR_FRUSTUM_PLANE].point = {0, 0, z_near};
+	frustrum_planes[NEAR_FRUSTUM_PLANE].normal.x = 0;
+	frustrum_planes[NEAR_FRUSTUM_PLANE].normal.y = 0;
+	frustrum_planes[NEAR_FRUSTUM_PLANE].normal.z = 1;
+
+	frustrum_planes[FAR_FRUSTUM_PLANE].point = {0, 0, z_far};
+	frustrum_planes[FAR_FRUSTUM_PLANE].normal.x = 0;
+	frustrum_planes[FAR_FRUSTUM_PLANE].normal.y = 0;
+	frustrum_planes[FAR_FRUSTUM_PLANE].normal.z = -1;
 }
-
-typedef struct 
-{
-	vec3_t point;
-	vec3_t normal;
-} plane_t;
